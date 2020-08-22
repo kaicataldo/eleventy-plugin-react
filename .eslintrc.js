5404e6bf48a8233f41ce0e90bbe23673c7d099c7
Kai Cataldo <kai@kaicataldo.com>
2020-08-22 14:00:03 -0400

    Add ability to mark components for hydration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,14 @@
 "use strict";
 
 module.exports = {
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "script",
+  },
   plugins: ["node"],
   extends: ["eslint:recommended", "plugin:node/recommended", "prettier"],
   rules: {
     strict: "error",
+    "node/no-unpublished-require": "off",
   },
 };
