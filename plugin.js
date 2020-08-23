aca83c77b27b7e2cb732cbe7a56b3307d51ca29e
Kai Cataldo <kai@kaicataldo.com>
2020-08-23 00:06:44 -0400

    Generate bundle with components marked for hydration

--- plugin.js
+++ plugin.js
@@ -4,40 +4,28 @@ const { addHook } = require("pirates");
 const React = require("react");
 const ReactDOM = require("react-dom/server");
 const babel = require("@babel/core");
-const babelConfig = require("./babelConfig");
-const hydrationCache = require("../hydration/hydrationCache");
-const { PACKAGE_ROOT, SUPPORTED_EXTENSIONS } = require("../utils").constants;
-
-function resolveModuleFromCWD(moduleToResolve) {
-  return require.resolve(moduleToResolve, { paths: [process.cwd()] });
-}
-
-function loadModuleFromCWD(modulePath) {
-  // TODO: Babel doesn't transform the `require` statements. Figure out module interop when using CommonJS in source code.
-  // loadedModule.__esModule === true ? loadedModule.default : loadedModule;
-  return require(resolveModuleFromCWD(modulePath));
-}
-
-function buildClientAssets(components) {
-  console.log(components);
-}
+const { bundleClientAssets } = require("./lib/clientBundler");
+const { PACKAGE_ROOT, SUPPORTED_EXTENSIONS } = require("./lib/utils/constants");
+const generateBabelConfig = require("./lib/utils/babelConfig");
+const { loadModuleFromCWD } = require("./lib/utils/moduleUtils");
 
 module.exports = function eleventyPluginReact(eleventyConfig) {
   const revertHook = addHook(
     (code, filename) => {
       const { code: compiledCode } = babel.transform(
-        `${code}\nexports.default.__modulePath = "${filename}"`,
+        `${code}\nif (exports.default) exports.default.__modulePath = "${filename}"`,
         {
           filename,
           cwd: PACKAGE_ROOT,
           babelrc: false,
-          ...babelConfig,
+          ...generateBabelConfig(),
         }
       );
       return compiledCode;
     },
     {
       exts: SUPPORTED_EXTENSIONS.map((ext) => `.${ext}`),
+      ignoreNodeModules: true,
     }
   );
 
@@ -49,19 +37,13 @@ module.exports = function eleventyPluginReact(eleventyConfig) {
       return loadModuleFromCWD(inputPath).default;
     },
     compile(_str, inputPath) {
-      return function render(data) {
+      return async function render(data) {
         const componentModule = loadModuleFromCWD(inputPath).default;
         const Component = React.createElement(componentModule, { data }, null);
         const html = ReactDOM.renderToString(Component);
-
         // Revert module hook after each page is rendered in case this a long-lived process.
         revertHook();
-
-        // Build client-side assets for partial hydration.
-        const components = hydrationCache.getComponents();
-        buildClientAssets(components);
-        hydrationCache.flush();
-
+        await bundleClientAssets();
         return html;
       };
     },
---
+++ utils.js
@@ -0,0 +1,7 @@
+"use strict";
+
+module.exports = {
+  withHydration: require("./lib/withHydration"),
+  React: require("react"),
+  ReactDOM: require("react-dom"),
+};
